0d7e078ff7e7beacf1370ad0f1de55069a9f4bdc
--add <sobayotoby@gmail.com>
2025-05-14 14:47:49 +0100

    made free-trial responsive

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Hero from '@/components/sections/Hero'
 import UseCase from "@/components/sections/UseCase"
 import Features from "@/components/sections/Features"
 import Pricing from "@/components/sections/Pricing"
-import FreeTrial from "@/components/sections/FreeTrial"
+// import FreeTrial from "@/components/sections/FreeTrial"
 // import FeatureNumbering from "@/components/feature-numbering"
 
 function App() {
